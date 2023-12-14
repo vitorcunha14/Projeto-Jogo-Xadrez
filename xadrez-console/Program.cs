cb688836eeb1203f5a88f6634bbb19a8de69a470
Vitor Cunha <vitorcunha1402@gmail.com>
2023-12-14 12:09:02 -0300

    Criada a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,6 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Posicao P = new Posicao(3, 4);
+            Console.WriteLine("Posiçao: " + P);
+
+            Console.ReadKey();
         }
     }
 }
